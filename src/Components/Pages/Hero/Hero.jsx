8d1024af192d3fce413eedace1b75beeef77bcab
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-02-24 23:50:02 +0600

    add image list

--- src/Components/Pages/Hero/Hero.jsx
+++ src/Components/Pages/Hero/Hero.jsx
@@ -4,7 +4,29 @@ import image3 from "../../../assets/hero/women.png";
 
 const Hero = () => {
 
-
+    const ImageList = [
+        {
+            id: 1,
+            img: image1,
+            title: "Upto 50% off on all Men's Wear",
+            description:
+                "lorem His Life will forever be Changed dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        },
+        {
+            id: 2,
+            img: image2,
+            title: "30% off on all Women's Wear",
+            description:
+                "Who's there lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        },
+        {
+            id: 3,
+            img: image3,
+            title: "70% off on all Products Sale",
+            description:
+                "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        },
+    ];
 
     return (
         <div>
