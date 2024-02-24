ad07f77cc2a8293906d9e642434043f341002025
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-02-25 00:27:53 +0600

    add hero design

--- src/Components/Pages/Hero/Hero.jsx
+++ src/Components/Pages/Hero/Hero.jsx
@@ -30,9 +30,28 @@ const Hero = () => {
 
     return (
         <div className="relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200">
-            <div className="h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z[8]">
+            <div className="h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z[8]"></div>
 
+
+            <div className="pb-8 sm:pb-0">
+                <div className="grid grid-cols-1  sm:grid-cols-2 px-3">
+                    <div className="flex  flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2  sm:order-1 relative  z-10">
+                        <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold">Lorem ipsum dolor.</h1>
+                        <p className="text-sm my-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quisquam neque praesentium. Expedita molestias dolore possimus nihil aperiam quos perferendis.</p>
+
+                        <div>
+                            <button className="bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full ml-1">Order now</button>
+                        </div>
+                    </div>
+
+                    <div className="order-1 sm:order-2 ">
+                        <div className="relative z-10 ">
+                            <img className="w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-125 object-contain mx-auto" src={image2} alt="" />
+                        </div>
+                    </div>
+                </div>
             </div>
+
         </div>
     );
 };
