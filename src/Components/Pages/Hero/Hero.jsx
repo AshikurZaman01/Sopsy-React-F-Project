7e306f1e51967cd032a91f601f4a910ad2d83c8c
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-02-25 00:02:57 +0600

    add hero bg

--- src/Components/Pages/Hero/Hero.jsx
+++ src/Components/Pages/Hero/Hero.jsx
@@ -29,8 +29,10 @@ const Hero = () => {
     ];
 
     return (
-        <div>
+        <div className="relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200">
+            <div className="h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z[8]">
 
+            </div>
         </div>
     );
 };
